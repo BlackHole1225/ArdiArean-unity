c03a09c82845608058c8b94b64d7bee53d1d46f9
Nagata Izanagi <153245896+BlackHole1225@users.noreply.github.com>
2024-07-17 10:53:08 +0900

    Update index.js

--- web3/index.js
+++ web3/index.js
@@ -8,7 +8,7 @@ document.body.appendChild(Object.assign(document.createElement("script"), { type
 // document.body.appendChild(Object.assign(document.createElement("script"), { type: "text/javascript", src: "https://unpkg.com/@toruslabs/torus-embed" }));
 // uncomment to enable walletconnect
 // document.body.appendChild(Object.assign(document.createElement("script"), { type: "text/javascript", src: "https://unpkg.com/@walletconnect/web3-provider@1.2.1/dist/umd/index.min.js" }));
-   
+      
 // load web3gl to connect to unity
 window.web3gl = {
     networkId: 0,
